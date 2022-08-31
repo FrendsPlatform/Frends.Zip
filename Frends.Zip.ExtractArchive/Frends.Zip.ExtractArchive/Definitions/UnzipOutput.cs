836e168f72ca1b9a5106aae68b256218d49107b9
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-08-31 12:03:12 +0300

    docs+cleanup

--- Frends.Zip.ExtractArchive/Frends.Zip.ExtractArchive/Definitions/UnzipOutput.cs
+++ Frends.Zip.ExtractArchive/Frends.Zip.ExtractArchive/Definitions/UnzipOutput.cs
@@ -9,6 +9,7 @@ public class UnzipOutput
     /// <summary>
     /// a List-object of extracted files.
     /// </summary>
+    /// <example>C:\temp\samplefile.txt"</example>
     public List<string> ExtractedFiles { get; set; }
 
     internal UnzipOutput()
